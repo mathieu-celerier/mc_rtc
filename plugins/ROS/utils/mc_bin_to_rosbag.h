a4648198a7136de03cfc66ab2af41c03cab99ff6
bastien-muraccioli <bastien.muraccioli.pro@gmail.com>
2024-07-22 14:47:10 +0900

    Remove unwanted files and update .gitignore

--- plugins/ROS/utils/mc_bin_to_rosbag.h
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright 2015-2019 CNRS-UM LIRMM, CNRS-AIST JRL
- */
-
-#pragma once
-
-#include <string>
-
-void mc_bin_to_rosbag(const std::string & in, const std::string & out, double dt);
